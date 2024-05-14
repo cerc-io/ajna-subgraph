0813d1ed863448aa91dc9f43b6295da2c146dd95
Prathamesh Musale <prathamesh.musale0@gmail.com>
2024-05-14 10:45:24 +0530

    Add pool address constants for filecoin network

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -38,6 +38,7 @@ poolInfoUtilsAddressTable.set('gnosis', Address.fromString('0x2baB4c287cF33a6eC3
 poolInfoUtilsAddressTable.set('blast', Address.fromString('0x6aF0363e5d2ddab4471f31Fe2834145Aea1E55Ee'))
 poolInfoUtilsAddressTable.set('goerli', Address.fromString('0xdE8D83e069F552fbf3EE5bF04E8C4fa53a097ee5'))
 poolInfoUtilsAddressTable.set('ganache', Address.fromString('0x6c5c7fD98415168ada1930d44447790959097482'))
+poolInfoUtilsAddressTable.set('filecoin', Address.fromString('0xCF7e3DABBaD8F0F3fdf1AE8a13C4be3872d06d56'))
 export const poolInfoUtilsMulticallAddressTable = new TypedMap<string, Address>()
 poolInfoUtilsMulticallAddressTable.set('mainnet', Address.fromString('0xe4e553243264f2bF7C135F1eC3a8c09078731227'))
 poolInfoUtilsMulticallAddressTable.set('arbitrum-one', Address.fromString('0xCcaf0542c78A3A5e55f99630a2B126A5BAA44FC3'))
@@ -48,6 +49,7 @@ poolInfoUtilsMulticallAddressTable.set('gnosis', Address.fromString('0x6A9142942
 poolInfoUtilsMulticallAddressTable.set('blast', Address.fromString('0x1307d1670746E6cF6377844D350DD412Ef1684E5'))
 poolInfoUtilsMulticallAddressTable.set('goerli', Address.fromString('0x63feF8659ECdC4F909ddFB55a8B701957115B906'))
 poolInfoUtilsMulticallAddressTable.set('ganache', Address.fromString('0x6548dF23A854f72335902e58a1e59B50bb3f11F1'))
+poolInfoUtilsMulticallAddressTable.set('filecoin', Address.fromString('0x82fba8E56F7D848B0Ee598f1449185b570d9B384'))
 export const positionManagerAddressTable = new TypedMap<string, Address>()
 positionManagerAddressTable.set('mainnet', Address.fromString('0x87B0F458d8F1ACD28A83A748bFFbE24bD6B701B1'))
 positionManagerAddressTable.set('arbitrum-one', Address.fromString('0x9A0BE971530Ed2B53597AC9155AC050ca1Bab7A3'))
@@ -58,6 +60,7 @@ positionManagerAddressTable.set('gnosis', Address.fromString('0x173b32A07b412969
 positionManagerAddressTable.set('blast', Address.fromString('0x2475d80b7634aC8F891a5D00c8b56AA3a40E4Cf7'))
 positionManagerAddressTable.set('goerli', Address.fromString('0x7b6C6917ACA28BA790837d41e5aA4A49c9Ad4570'))
 positionManagerAddressTable.set('ganache', Address.fromString('0xdF7403003a16c49ebA5883bB5890d474794cea5a'))
+positionManagerAddressTable.set('filecoin', Address.fromString('0x0cEfA3be6496B8Ab0A66B01aABEf05A5aE38221b'))
 
 // GrantFund constants
 export const CHALLENGE_PERIOD_LENGTH    = BigInt.fromI32(50400)
